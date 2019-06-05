c6243e6dc6330ba27b5e45beaab15f1c6a59334f
Minori Hashimoto <minori.fh@gmail.com>
2019-06-04 17:04:47 -0700

    add displayGif function

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -5,6 +5,7 @@ var initialFeels = ["chill", "mad", "weird"];
 
 //Create initial variables
 var counter = 0; 
+var apiKey = "KkaHsdv8FJliwVprKPfmVBbYGteOAFef"
 
 //FUNCTION DECLARATIONS
 //function: to display initial emotion arrray as buttons
@@ -21,7 +22,27 @@ function renderButtons(){
     }
 };
 
-//function: display feeling gif
+//function: display feeling gifs
+function displayGif(){
+    counter++ //incremental increase of counter
+    var feels = $(this).attr("data-name"); //store emotion clicked on by user to the variable "feels"
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + feels + "&api_key=" + apiKey + "&limit=10"
+
+    if (counter === 1){
+        // Create new divs to place the gif header
+        var r1 = $("<div>"); //create an empty div
+        r1.addClass("gif-header-row")
+        r1.text("I'm feeling: " + feels)
+
+        $("#FEELS").prepend(r1);
+    }
+
+
+
+
+
+
+};
 
 //Event handler: when user clicks on "inititate feels" button 
 $("#add-feels").on("click", function(event){
@@ -31,6 +52,9 @@ $("#add-feels").on("click", function(event){
     initialFeels.push(feels);
     renderButtons();
 });
+
+//Event handler: when user clicks on a button with the class "emotions"
+$(document).on("click",".emotion",displayGif);
 
 renderButtons();
 
